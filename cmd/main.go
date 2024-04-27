d37f9aa244681145eb5482ea8dfafcdf7f2eddeb
Taylor <taylor.r.meador@gmail.com>
2024-04-27 13:19:11 -0600

    Play sound

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+	"time"
+
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	f, err := os.Open("./cmd/msmw.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer streamer.Close()
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	speaker.Play(streamer)
+	select {}
 }
