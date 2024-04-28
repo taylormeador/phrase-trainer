019166bb2879b933a6ca1d801add7ffaa2c4f94b
Taylor <taylor.r.meador@gmail.com>
2024-04-28 15:17:56 -0600

    Another dead end

--- cmd/main.go
+++ cmd/main.go
@@ -3,33 +3,25 @@ package main
 import (
 	"log"
 	"os"
+	"time"
 
 	"github.com/200sc/klangsynthese/audio/filter"
 	kmp3 "github.com/200sc/klangsynthese/mp3"
+	"github.com/gopxl/beep"
+	"github.com/gopxl/beep/mp3"
+	"github.com/gopxl/beep/speaker"
 )
 
-// func createSnippet(s beep.Streamer, f beep.Format, start int, end int, loops int, ratio float64) beep.Streamer {
-// 	buffer := beep.NewBuffer(f)
-// 	buffer.Append(s)
+func createSnippet(s beep.Streamer, f beep.Format, start int, end int, loops int, ratio float64) beep.Streamer {
+	buffer := beep.NewBuffer(f)
+	buffer.Append(s)
 
-// 	half := buffer.Streamer(buffer.Len()/start, buffer.Len()/end)
-// 	loop := beep.Loop(loops, half)
+	half := buffer.Streamer(buffer.Len()/start, buffer.Len()/end)
+	loop := beep.Loop(loops, half)
 
-// 	fast := beep.ResampleRatio(4, ratio, loop)
+	fast := beep.ResampleRatio(4, ratio, loop)
 
-// 	return fast
-// }
-
-func main() {
-	f, err := os.Open("./cmd/free_shevacadoo.mp3")
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// _, format, err := mp3.Decode(f)
-	// if err != nil {
-	// 	log.Fatal(err)
-	// }
+	// TODO: save file to disk? How to hack this?
 
 	snippet, err := kmp3.Load(f)
 	if err != nil {
@@ -42,24 +34,37 @@ func main() {
 	}
 	encoding := ps.PitchShift(0.5)
 
-	// encoding := filter.Speed(2, ps)
-
 	snippet, err = snippet.Filter(encoding)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	snippet.Play()
+	return fast
+}
 
-	select {}
+func main() {
+	f, err := os.Open("./cmd/free_shevacadoo.mp3")
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-	// done := make(chan bool)
-	//
-	//	speaker.Play(beep.Seq(snippet, beep.Callback(func() {
-	//		done <- true
-	//	})))
-	//
-	// <-done
+	start := 4
+	end := 2
+	loops := 2
+	ratio := 1.1
+	snippet := createSnippet(streamer, format, start, end, loops, ratio)
+
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(snippet, beep.Callback(func() {
+		done <- true
+	})))
+
+	<-done
 }
