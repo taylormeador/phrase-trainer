a7102183fe9a06bb46ef1649ea1caac447e60824
Taylor <taylor.r.meador@gmail.com>
2024-04-27 18:01:38 -0600

    Play noise for 5 seconds only

--- cmd/main.go
+++ cmd/main.go
@@ -10,6 +10,10 @@ import (
 func main() {
 	sr := beep.SampleRate(44100)
 	speaker.Init(sr, sr.N(time.Second/10))
-	speaker.Play(Noise{})
-	select {}
+
+	done := make(chan bool)
+	speaker.Play(beep.Seq(beep.Take(sr.N(5*time.Second), Noise{}), beep.Callback(func() {
+		done <- true
+	})))
+	<-done
 }
