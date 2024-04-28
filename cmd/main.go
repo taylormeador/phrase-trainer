b65d73d38952bf5c4759b3fbd98c6de5e5eedb1f
Taylor <taylor.r.meador@gmail.com>
2024-04-28 13:11:46 -0600

    Abandoned attempt to use github.com/200sc/klangsynthese

--- cmd/main.go
+++ cmd/main.go
@@ -3,24 +3,22 @@ package main
 import (
 	"log"
 	"os"
-	"time"
 
-	"github.com/gopxl/beep"
-	"github.com/gopxl/beep/mp3"
-	"github.com/gopxl/beep/speaker"
+	"github.com/200sc/klangsynthese/audio/filter"
+	kmp3 "github.com/200sc/klangsynthese/mp3"
 )
 
-func createSnippet(s beep.Streamer, f beep.Format, start int, end int, loops int, ratio float64) beep.Streamer {
-	buffer := beep.NewBuffer(f)
-	buffer.Append(s)
+// func createSnippet(s beep.Streamer, f beep.Format, start int, end int, loops int, ratio float64) beep.Streamer {
+// 	buffer := beep.NewBuffer(f)
+// 	buffer.Append(s)
 
-	half := buffer.Streamer(buffer.Len()/start, buffer.Len()/end)
-	loop := beep.Loop(loops, half)
+// 	half := buffer.Streamer(buffer.Len()/start, buffer.Len()/end)
+// 	loop := beep.Loop(loops, half)
 
-	fast := beep.ResampleRatio(4, ratio, loop)
+// 	fast := beep.ResampleRatio(4, ratio, loop)
 
-	return fast
-}
+// 	return fast
+// }
 
 func main() {
 	f, err := os.Open("./cmd/free_shevacadoo.mp3")
@@ -28,22 +26,39 @@ func main() {
 		log.Fatal(err)
 	}
 
-	streamer, format, err := mp3.Decode(f)
+	// _, format, err := mp3.Decode(f)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	snippet, err := kmp3.Load(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	snippet.Play()
+
+	ps, err := filter.NewFFTShifter(2, 1)
+	if err != nil {
+		log.Fatal(err)
+	}
+	encoding := ps.PitchShift(1.5)
+
+	snippet, err = snippet.Filter(encoding)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	start := 4
-	end := 2
-	loops := 2
-	ratio := 1.1
-	snippet := createSnippet(streamer, format, start, end, loops, ratio)
+	// snippet.Play()
 
-	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	select {}
 
-	done := make(chan bool)
-	speaker.Play(beep.Seq(snippet, beep.Callback(func() {
-		done <- true
-	})))
-	<-done
+	// 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+
+	// done := make(chan bool)
+	//
+	//	speaker.Play(beep.Seq(snippet, beep.Callback(func() {
+	//		done <- true
+	//	})))
+	//
+	// <-done
 }
--- go.mod
+++ go.mod
@@ -2,12 +2,15 @@ module phrasetrainer.taylormeador.com
 
 go 1.22.0
 
-require github.com/gopxl/beep v1.4.1
+require (
+	github.com/200sc/klangsynthese v0.2.1
+	github.com/gopxl/beep v1.4.1
+)
 
 require (
-	github.com/ebitengine/oto/v3 v3.1.0 // indirect
-	github.com/ebitengine/purego v0.7.1 // indirect
 	github.com/hajimehoshi/go-mp3 v0.3.4 // indirect
+	github.com/oov/directsound-go v0.0.0-20141101201356-e53e59c700bf // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	golang.org/x/sys v0.12.0 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/yobert/alsa v0.0.0-20230126204319-85bb7ee02e5b // indirect
 )
