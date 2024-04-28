9d75220335a38dd5d0d3797d4fdb069d66630f68
Taylor <taylor.r.meador@gmail.com>
2024-04-27 18:02:35 -0600

    Use StreamerFunc instead of Streamer

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,29 @@
 package main
 
 import (
+	"math/rand"
 	"time"
 
 	"github.com/gopxl/beep"
 	"github.com/gopxl/beep/speaker"
 )
 
+func Noise() beep.Streamer {
+	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
+		for i := range samples {
+			samples[i][0] = rand.Float64()*2 - 1
+			samples[i][1] = rand.Float64()*2 - 1
+		}
+		return len(samples), true
+	})
+}
+
 func main() {
 	sr := beep.SampleRate(44100)
 	speaker.Init(sr, sr.N(time.Second/10))
 
 	done := make(chan bool)
-	speaker.Play(beep.Seq(beep.Take(sr.N(5*time.Second), Noise{}), beep.Callback(func() {
+	speaker.Play(beep.Seq(beep.Take(sr.N(5*time.Second), Noise()), beep.Callback(func() {
 		done <- true
 	})))
 	<-done
