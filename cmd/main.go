b07a141c247a6d99baf2fb52b26ba4eaac9334c6
Taylor <taylor.r.meador@gmail.com>
2024-04-27 18:15:29 -0600

    Allow user to add songs to queue

--- cmd/main.go
+++ cmd/main.go
@@ -1,30 +1,49 @@
 package main
 
 import (
-	"math/rand"
+	"fmt"
+	"os"
 	"time"
 
 	"github.com/gopxl/beep"
+	"github.com/gopxl/beep/mp3"
 	"github.com/gopxl/beep/speaker"
 )
-
-func Noise() beep.Streamer {
-	return beep.StreamerFunc(func(samples [][2]float64) (n int, ok bool) {
-		for i := range samples {
-			samples[i][0] = rand.Float64()*2 - 1
-			samples[i][1] = rand.Float64()*2 - 1
-		}
-		return len(samples), true
-	})
-}
 
 func main() {
 	sr := beep.SampleRate(44100)
 	speaker.Init(sr, sr.N(time.Second/10))
 
-	done := make(chan bool)
-	speaker.Play(beep.Seq(beep.Take(sr.N(5*time.Second), Noise()), beep.Callback(func() {
-		done <- true
-	})))
-	<-done
+	// A zero Queue is an empty Queue.
+	var queue Queue
+	speaker.Play(&queue)
+
+	for {
+		var name string
+		fmt.Print("Type an MP3 file name: ")
+		fmt.Scanln(&name)
+
+		// Open the file on the disk.
+		f, err := os.Open(name)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// Decode it.
+		streamer, format, err := mp3.Decode(f)
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+
+		// The speaker's sample rate is fixed at 44100. Therefore, we need to
+		// resample the file in case it's in a different sample rate.
+		resampled := beep.Resample(4, format.SampleRate, sr, streamer)
+
+		// And finally, we add the song to the queue.
+		speaker.Lock()
+		queue.Add(resampled)
+		speaker.Unlock()
+	}
 }
