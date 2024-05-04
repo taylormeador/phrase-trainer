3351c2bdb8b44bad402cdd41a461127532c0e742
Taylor <taylor.r.meador@gmail.com>
2024-05-04 16:42:31 -0600

    Add comments

--- cmd/main.go
+++ cmd/main.go
@@ -24,23 +24,34 @@ func createSnippet(s beep.Streamer, f beep.Format, start int, end int, loops int
 	return fast
 }
 
+// Want to flesh out functionality of creating a snippet. Basically everything a user has to do before we send the audio to Python
+// 1. User uploads their own mp3
+// 2. User plays their mp3 back
+// 3. User seeks the mp3 and finds a section they want to create a snippet from
+// 4. User runs command `createsnippet <song name> <start timestamp> <end timestamp>`
+// 5. Go calls Python script that slices the audio, creates copies at different speeds, and writes them to blob? local?
+
 func main() {
+	// Open file
 	f, err := os.Open("./cmd/free_shevacadoo.mp3")
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Decode audio to Streamer
 	streamer, format, err := mp3.Decode(f)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	// Create the snippet with parameters
 	start := 4
 	end := 2
 	loops := 2
 	ratio := 1.1
 	snippet := createSnippet(streamer, format, start, end, loops, ratio)
 
+	// Play
 	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
 
 	done := make(chan bool)
