9ad76a177b83bc672e37d7f6a8556498320ce8f5
Taylor <taylor.r.meador@gmail.com>
2024-05-08 20:12:20 -0600

    Insert Upload model

--- cmd/main.go
+++ cmd/main.go
@@ -1,98 +1,56 @@
 package main
 
 import (
-	"bufio"
 	"context"
-	"fmt"
-	"log"
+	"log/slog"
 	"os"
-	"strings"
+	"time"
 
 	crdbpgx "github.com/cockroachdb/cockroach-go/v2/crdb/crdbpgxv5"
 	"github.com/jackc/pgx/v5"
-	"phrasetrainer.tm.com/internal/aws/s3utils"
-	"phrasetrainer.tm.com/internal/db"
+	"phrasetrainer.tm.com/internal/data"
 )
 
-// Want to flesh out functionality of creating a phrase. Basically everything a user has to do before we send the audio to Python
-// 1. User uploads their own mp3 ***DONE***
-// 2. User plays their mp3 back
-// 3. User seeks the mp3 and finds a section they want to create a phrase from
-// 4. User runs command `createphrase <song name> <start timestamp> <end timestamp>`
-// 5. Go calls Python script that slices the audio, creates copies at different speeds, and writes them to blob? local?
-
 func main() {
+	// Create a logger.
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
-	// Connect to database and create table
-	conn := db.Connect()
+	// Open the DB connection.
+	conn, err := openConn()
+	if err != nil {
+		logger.Error(err.Error())
+	}
 	defer conn.Close(context.Background())
+	logger.Info("Successfully connected to db")
 
-	err := crdbpgx.ExecuteTx(context.Background(), conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
-		return db.InitTable(context.Background(), tx)
+	// Create an Upload.
+	upload := &data.Upload{ID: 0, UserID: 1, Timestamp: time.Now(), FileName: "file name", FileLabel: "file label", BlobName: "asdfwf-asdfawef-asdfawef-awsefasef"}
+
+	// Insert the Upload.
+	models := data.NewModels(conn)
+	err = crdbpgx.ExecuteTx(context.Background(), conn, pgx.TxOptions{}, func(tx pgx.Tx) error {
+		return models.Uploads.Insert(context.Background(), tx, upload)
 	})
 	if err != nil {
-		log.Fatal(err)
+		logger.Error(err.Error())
 	}
 
-	// Loop and get commands from user
-	r := bufio.NewReader(os.Stdin)
+	logger.Info("Insert was successful")
 
-	for {
-		// Parse user input into variables
-		fmt.Print("-> ")
-		command, args := parseInput(r)
-
-		client := s3utils.GetS3Client()
-
-		switch command {
-		case "upload":
-			err := Upload(client, conn, args)
-			if err != nil {
-				log.Fatal(err)
-			}
-		case "play":
-			err := Play(client, conn, args)
-			if err != nil {
-				log.Fatal(err)
-			}
-		case "list":
-			err := List(client)
-			if err != nil {
-				log.Fatal(err)
-			}
-		case "delete-all-s3-objects":
-			err := DeleteAll(client)
-			if err != nil {
-				log.Fatal(err)
-			}
-		case "exit":
-			fmt.Println("Bye!")
-			return
-		default:
-			fmt.Println("Command not recognized")
-		}
-
-	}
 }
 
-func parseInput(r *bufio.Reader) (string, []string) {
-	var input string
-	var command string
-	var args []string
+func openConn() (*pgx.Conn, error) {
+	config, err := pgx.ParseConfig(os.Getenv("COCKROACH_DB_DSN"))
+	if err != nil {
+		return nil, err
+	}
 
-	input, _ = r.ReadString('\n')
+	config.RuntimeParams["phrase_trainer"] = "$ docs_simplecrud_gopgx"
 
-	// Make CR LF into LF
-	input = strings.Replace(input, "\n", "", -1)
+	conn, err := pgx.ConnectConfig(context.Background(), config)
+	if err != nil {
+		return nil, err
+	}
 
-	words := strings.Split(input, " ")
-
-	// Commands will always be one word (the first word)
-	// followed by any number of arguments (if any)
-	command = words[0]
-	args = words[1:]
-
-	// TODO: validate `command` and `args`
-
-	return command, args
+	return conn, nil
 }
